243da7a7ab61748239bfd89ab4a9dffc612d4cf8
Exanderous <saadt6030@gmail.com>
2026-01-01 22:12:27 +0500

    Revert 403 bypass changes in dev config

--- cypress/config/cypress.dev.js
+++ cypress/config/cypress.dev.js
@@ -6,19 +6,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.DEV_BASE_URL,
-    supportFile: "cypress/support/e2e.js",
-    chromeWebSecurity: false,
-    setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser, launchOptions) => {
-        if (browser.name === 'chrome' || browser.name === 'chromium') {
-          launchOptions.args.push('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
-        }
-        return launchOptions;
-      });
-      on('uncaught:exception', (err, runnable) => {
-        return false;
-      });
-    }
+    supportFile: "cypress/support/e2e.js"
   },
   env: {
     baseUrl: process.env.DEV_BASE_URL,
